6e473805bee57a6c7170b593987ef3ad5f470d50
JessyJP <49342341+JessyJP@users.noreply.github.com>
2023-05-11 23:24:41 +0100

    Test Files

---
+++ test.py
@@ -0,0 +1,30 @@
+import subprocess
+
+# Variables
+scriptName = './text_replacement_file_processor.py'
+
+# Define the command to be executed
+command = []
+
+command.append("")
+command.append("-h")
+
+# Replace the word 'Hello' with 'Hi' in all files
+command.append(' -t Hello -s Hi -i test_dir')
+
+# Replace the word 'Hello' with 'Hi' in all files, but don't write the changes to the files
+command.append(' -v -t Hello -s Hi -i test_dir')
+
+# Replace the word 'Hello' with 'Hi' in all files, print detailed output
+command.append(' -d -t Hello -s Hi -i test_dir')
+
+# Replace the word 'Hello' with 'Hi' in file5.txt only
+command.append(' -t Hello -s Hi -i test_dir/file5.txt')
+
+# Replace multiple words in all files
+command.append(' -t Hello test -s Hi exam -i test_dir')
+
+# Execute the command
+for cmd in command:
+    cmd = ['python', scriptName] + cmd.split()
+    subprocess.run(cmd)
